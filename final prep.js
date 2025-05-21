294c8e3e67cecbe84efb0fe0f5072eb9ae4d835a
Caitlin Malan <cshanmalan28@gmail.com>
2025-05-21 18:29:24 +0200

    Deleted .js file

--- final prep.js
+++
@@ -1,9 +0,0 @@
-let names = ["John", "Alice", "Bob"];
-let targetName = "Alice";
-
-for (let i = 0; i < names.length; i++) {
-    if (names[i] === targetName) {
-        console.log(`${targetName} is in the array at index ${i}.`);
-        break;
-    }
-}
